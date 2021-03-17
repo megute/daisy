f6b6f92265ea0891ccc02e2f10b1f9cf18119f8e
megute <alinkapirjinka@gmail.com>
2021-03-17 21:27:29 +0200

    Доработала адаптив

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 var mixer = mixitup ( '.container-filter' );
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.head__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
